53d6a8a4e133ac32de0b1272b23cafd91112ec9e
Peter Chen <xtiqin@163.com>
2022-10-05 12:30:41 +0800

    Optimize key encoder

--- examples/test_encode_key/main.c
+++ examples/test_encode_key/main.c
@@ -7,8 +7,16 @@
 
 
 int main() {
-    printf("code: %lld\n", rml_encode_key("hello", 101));
-    printf("code: %lld\n", rml_encode_key("elllo", 101));
-    printf("code: %lld\n", rml_encode_key("hello1", 101));
+    int64_t prime_number = 29;
+    printf("code: %lld\n", rml_encode_key("hello", prime_number));
+    printf("code: %lld\n", rml_encode_key("elllo", prime_number));
+    printf("code: %lld\n", rml_encode_key("hello1", prime_number));
+    printf("code: %lld\n", rml_encode_key("汉字", prime_number));
+    printf("code: %lld\n", rml_encode_key("字汉", prime_number));
+    printf("code: %lld\n", rml_encode_key("汉字字符", prime_number));
+    printf("code: %lld\n", rml_encode_key("字符汉字", prime_number));
+    printf("code: %lld\n", rml_encode_key("日本語", prime_number));
+    printf("code: %lld\n", rml_encode_key("日語本", prime_number));
+    printf("code: %lld\n", rml_encode_key("한국", prime_number));
     return 0;
 }
--- rmlcore/source/key_utils.c
+++ rmlcore/source/key_utils.c
@@ -7,15 +7,24 @@
 
 int64_t rml_encode_key(char *key, int64_t prime_number) {
     int64_t length = strlen(key);
-    int64_t code1 = 0;
-    int64_t code2 = 0;
+    int64_t code1 = 1;
+    int64_t code2 = 1;
+    int64_t code3 = 1;
+    int64_t code4 = 1;
+    uint8_t current = 0;
     for (int i = 0; i < length; ++i) {
-        if (i % 2) {
-            code1 += key[i];
-        } else {
-            code2 += key[i];
+        current = key[i];
+        code1 += current;
+        if (i % 2 == 0) {
+            code2 += current;
+        }
+        if (i % 3 == 0) {
+            code3 += current;
+        }
+        if (i % 4 == 0) {
+            code4 += current;
         }
     }
-    int64_t code = (code1 * code2) % prime_number;
+    int64_t code = (code1 * code2 * code3 * code4) % prime_number;
     return code;
 }
